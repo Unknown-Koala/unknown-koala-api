234295e59208417625cdaf8fddd316fdd88bbe9c
Julia Camino <caminogjulia@gmail.com>
2023-10-31 18:51:30 -0400

    Configure the API

--- Unknown.Koala.Data/StoreContext.cs
+++ Unknown.Koala.Data/StoreContext.cs
@@ -5,8 +5,7 @@ namespace Unknown.Koala.Data
 {
     public class StoreContext : DbContext
     {
-        public StoreContext(DbContextOptions<StoreContext> options)
-        : base(options)
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
         { }
 
         public DbSet<Item> Items { get; set; }
