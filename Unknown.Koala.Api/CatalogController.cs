fcd2fdd13d1be28676b9e0a05a73aa00a0e2991f
Julia Camino <caminogjulia@gmail.com>
2023-11-02 15:11:37 -0400

    Adding Post

--- Unknown.Koala.Api/CatalogController.cs
+++ Unknown.Koala.Api/CatalogController.cs
@@ -34,7 +34,9 @@ namespace Unknown.Koala.Api.Controllers
         [HttpPost]
         public IActionResult Post(Item item)
         {
-            return Created("/catalog/42", item);
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return Created($"/catalog/{item.Id}", item);
         }
 
         [HttpPost("{id:int}/ratings")]
