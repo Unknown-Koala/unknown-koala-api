8b0733f4a7495380b46394981923fa2d8df7e83b
Julia Camino <caminogjulia@gmail.com>
2023-11-28 18:24:08 -0500

    CORS solution

--- Unknown.Koala.Api/Program.cs
+++ Unknown.Koala.Api/Program.cs
@@ -12,7 +12,15 @@ builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlite("Data Source=../Registrar.sqlite",
     b => b.MigrationsAssembly("Unknown.Koala.Api"))
 );
-
+builder.Services.AddCors(options => 
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -26,6 +34,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
