e1a6238e1094530bbae68009ff80569ac143c526
Julia Camino <caminogjulia@gmail.com>
2023-11-28 20:16:09 -0500

    Authority & Audience

---
+++ Unknown.Koala.Api/Security/HasScopeHandler.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Unknown.Koala.Api.Security
+{
+    public class HasScopeHandler : AuthorizationHandler<HasScopeRequirements>
+    {
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context, HasScopeRequirements requirement)
+        {
+            if(!context.User.HasClaim(c => c.Type == "Scope" && c.Issuer == requirement.Issuer))
+                return Task.CompletedTask;
+            
+            var scopes = context.User
+                .FindFirst(c => c.Type == "Scope" && c.Issuer == requirement.Issuer)
+                .Value.Split(' ');
+
+            if(scopes.Any(s => s == requirement.Scope))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
